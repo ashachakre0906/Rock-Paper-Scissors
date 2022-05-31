ed7d992ae245c8c484a97ff1d47b789231f3d4a0
ashachakre0906 <chourpagar.asha@gmail.com>
2022-05-31 16:35:01 -0700

    Fixed the typo

--- js/script.js
+++ js/script.js
@@ -12,6 +12,24 @@ var gameStart = function(){
 }
 var index = Math.floor(Math.random()* option.length);
 var compChoices = option[index];//get random index from array of options
+window.alert("The computer chose " + compChoices);
+if (userChoices === compChoices){
+    ties++;
+    window.alert("its a tie!");
+}else if(
+     (userChoices === "R" && compChoices === "S" )|| 
+     (userChoices === "P" && compChoices === "R" )||
+     (userChoices === "S" && compChoices === "P" )
+
+){
+    win++;
+    window.alert("You win");
+} else {
+   losses++;
+   window.alert("You lost!");
+}
+
+
 
 
 
