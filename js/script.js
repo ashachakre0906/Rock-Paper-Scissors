9d0b50a59e383f22d8f62255bf225be494fbc7cc
ashachakre0906 <chourpagar.asha@gmail.com>
2022-05-31 16:15:18 -0700

    created gamestart function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+//Defined and Assigned value to the variable 
 var win = 0;
 var ties = 0;
 var losses = 0;
+
+var compChoice = ["R","P","S"];
+var gameStart = function(){
+    var userChoices =window.prompt("Please enter R, P, S" );
+    if (!userChoices){
+        return;//if user presses cancel end the game
+    }
+}
+var index = Math.floor(Math.random()* compChoice.length);
+
